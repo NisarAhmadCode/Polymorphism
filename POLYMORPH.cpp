e64ca4bdc07ca456bfef17180bc15a92db0c97f6
Nisar Ahmad <na4609594@gmail.com>
2025-06-14 20:11:26 +0500

    Add files via upload

---
+++ POLYMORPH.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Person{
+private:
+char name[50];
+public:
+void get()
+{
+cout<<"Enter Your Name:";
+cin>>name;
+}
+void show()
+{ 
+cout<<"Your Name="<<name<<endl;
+}
+};
+int main()
+{
+Person *ptr[5];
+int i;
+for(i=0;i<5;i++){
+ptr[i]=new Person;
+ptr[i]->get();
+}
+ for(i=0;i<5;i++)
+ptr[i]->show();
+}
+
